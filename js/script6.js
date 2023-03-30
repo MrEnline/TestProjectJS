9f099e32db69ca5527bbe8ae5ca31cb833395f38
Sergey Kuznetsov <mr.enline@gmail.com>
2023-03-31 00:14:43 +0300

    new tasks

--- js/script6.js
+++ js/script6.js
@@ -2,7 +2,7 @@
 //Частный случай анаграмма-перевертыш
 //На входе массив слов ['пот', 'лом', 'мол', 'зов', 'воз', 'топ'] на выходе [['лом', 'мол'],['зов', 'воз']]
 
-const arrWords = ["пот", "лом", "ток", "мол", "зов", "воз", "кот", "топ", "апорт", "тропа"];
+const arrWords = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'кот', 'топ', 'апорт', 'тропа'];
 
 const obj = arrWords.reduce((result, curr, index) => {
     result[curr] = index;
@@ -10,7 +10,7 @@ const obj = arrWords.reduce((result, curr, index) => {
 }, {});
 
 const anagramms = arrWords.reduce((result, curr, index) => {
-    const reverseWord = curr.split("").reverse().join("");
+    const reverseWord = curr.split('').reverse().join('');
     if (obj[reverseWord] > -1 && index !== obj[reverseWord]) {
         obj[curr] = obj[reverseWord];
         result.push([curr, reverseWord]);
@@ -21,10 +21,10 @@ const anagramms = arrWords.reduce((result, curr, index) => {
 console.log(anagramms);
 
 //Общий случай с анаграммами, когда из слова можно получить другое слово
-const arrWords1 = ["пот", "лом", "ток", "мол", "зов", "воз", "озв", "кот", "топ", "апорт", "тропа", "дерево"];
+const arrWords1 = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'озв', 'кот', 'топ', 'апорт', 'тропа', 'дерево'];
 const anagramms1 = arrWords1
     .map((value) => {
-        return value.split("").sort().join("");
+        return value.split('').sort().join('');
     })
     .reduce((result, curr, index, arr) => {
         const findInd = arr.findIndex((value) => value === curr);
@@ -34,11 +34,11 @@ const anagramms1 = arrWords1
 console.log(anagramms1);
 
 //Хавролевский вариант
-const arrWords2 = ["пот", "лом", "ток", "мол", "зов", "воз", "кот", "топ", "корыто", "озв"];
+const arrWords2 = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'кот', 'топ', 'корыто', 'озв'];
 
 function getAnograms(array) {
     const anograms = array.reduce((acc, word) => {
-        const orderedCharacters = word.split("").sort().join("");
+        const orderedCharacters = word.split('').sort().join('');
 
         if (acc[orderedCharacters]) {
             acc[orderedCharacters].push(word);
@@ -55,11 +55,11 @@ function getAnograms(array) {
 console.log(getAnograms(arrWords2));
 
 //Общий случай с анаграммами, когда из слова можно получить другое слово
-const arrWords3 = ["пот", "лом", "ток", "мол", "зов", "воз", "озв", "кот", "топ", "апорт", "тропа", "дерево"];
+const arrWords3 = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'озв', 'кот', 'топ', 'апорт', 'тропа', 'дерево'];
 
 const anagramms3 = arrWords3
     .map((value) => {
-        return value.split("").sort().join("");
+        return value.split('').sort().join('');
     })
     .sort();
 console.log(anagramms3);
@@ -67,15 +67,15 @@ console.log(anagramms3);
 //Задача № 2.
 //Палиндром. Проверить фразу на палиндром
 
-const word = "калаш";
-const isPalindrom = word === word.split("").reverse().join("");
-console.log(isPalindrom ? "Слово-палиндром" : "Слово не является палиндромом");
+const word = 'калаш';
+const isPalindrom = word === word.split('').reverse().join('');
+console.log(isPalindrom ? 'Слово-палиндром' : 'Слово не является палиндромом');
 
 //второй вариант
-const word1 = "Лёша на полке клопа нашёл";
-const formatWord = word1.toLowerCase().split(" ").join("");
-const isPalindrom1 = formatWord === formatWord.split("").reverse().join("");
-console.log(isPalindrom1 ? "Фраза-палиндром" : "Фраза не является палиндромом");
+const word1 = 'Лёша на полке клопа нашёл';
+const formatWord = word1.toLowerCase().split(' ').join('');
+const isPalindrom1 = formatWord === formatWord.split('').reverse().join('');
+console.log(isPalindrom1 ? 'Фраза-палиндром' : 'Фраза не является палиндромом');
 
 //Задача № 3.
 //(03).Купюры;
@@ -153,21 +153,84 @@ console.log(getExcangeBancknotes(sum, banknotes1));
 
 //5-я задача 'AAABBBCCD'=>'A3B3C2D'
 
-str = "AAABBBBCCD";
+str = 'AAABBBBCCDААFKKKD';
 function getNumberRepeatSymbols(str) {
-    const objSymbols = str.split("").reduce((result, symbol) => {
-        if (result[symbol]) {
-            result[symbol] = result[symbol] + 1;
-        } else {
-            result[symbol] = 1;
-        }
-        return result;
-    }, {});
-    return Object.entries(objSymbols)
-        .map(([key, value]) => {
-            return key + value;
+    const objSymbols = str.split('').reduce(
+        (result, symbol, index, arr) => {
+            index !== 0 && arr[index] !== arr[index - 1] ? result.indexArr++ : result.indexArr;
+            if (index === 0 || arr[index] !== arr[index - 1]) {
+                result[result.indexArr] = [symbol];
+            } else {
+                result[result.indexArr].push(symbol);
+            }
+            return result;
+        },
+        { indexArr: 0 }
+    );
+    delete objSymbols.indexArr;
+    return Object.values(objSymbols)
+        .map((value) => {
+            return value.length > 1 ? value[0] + value.length : value[0];
         })
-        .join("");
+        .join('');
 }
 
 console.log(getNumberRepeatSymbols(str));
+
+//второе решение попроще
+function getNumberRepeatSymbols1(str) {
+    const arr = str.split('');
+    let count = 1;
+    let newStr = '';
+    for (let i = 0; i < arr.length; i++) {
+        if (i + 1 < arr.length && arr[i] === arr[i + 1]) {
+            count = count + 1;
+        } else {
+            count !== 1 ? (newStr = newStr + arr[i] + count) : (newStr = newStr + arr[i]);
+            count = 1;
+        }
+    }
+    return newStr;
+}
+
+console.log(getNumberRepeatSymbols1(str));
+
+//третье решение попроще
+function getNumberRepeatSymbols2(str) {
+    const arr = str.split('');
+    let count = 1;
+    for (let i = 0; i < arr.length; i++) {
+        if (i + 1 < arr.length && arr[i] === arr[i + 1]) {
+            arr[i] = '';
+            count = count + 1;
+        } else {
+            if (count !== 1) {
+                arr[i] = arr[i] + count;
+            }
+            count = 1;
+        }
+    }
+    return arr.join('');
+}
+
+console.log(getNumberRepeatSymbols2(str));
+
+//'[()]' = true
+//'[(])' = false
+
+str = '[[(((()))])]';
+
+function checkString(str) {
+    const arrSymbols = str.split('');
+    if (arrSymbols.length % 2 > 0) return false;
+
+    for (let i = arrSymbols.length / 2 - 1; i >= 0; i--) {
+        const left = arrSymbols[i];
+        const right = arrSymbols[arrSymbols.length - 1 - i];
+        if (!(left + right == '[]' || left + right == '()')) return false;
+    }
+
+    return true;
+}
+
+console.log(checkString(str));
