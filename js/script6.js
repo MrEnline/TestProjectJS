3980f980c6d0f44c4324c3b5767d9a8ce11b2c8a
Kuznetsov Sergey <mr.enline@gmail.com>
2023-03-30 20:39:43 +0300

    new tasks

--- js/script6.js
+++ js/script6.js
@@ -2,7 +2,7 @@
 //Частный случай анаграмма-перевертыш
 //На входе массив слов ['пот', 'лом', 'мол', 'зов', 'воз', 'топ'] на выходе [['лом', 'мол'],['зов', 'воз']]
 
-const arrWords = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'кот', 'топ', 'апорт', 'тропа'];
+const arrWords = ["пот", "лом", "ток", "мол", "зов", "воз", "кот", "топ", "апорт", "тропа"];
 
 const obj = arrWords.reduce((result, curr, index) => {
     result[curr] = index;
@@ -10,7 +10,7 @@ const obj = arrWords.reduce((result, curr, index) => {
 }, {});
 
 const anagramms = arrWords.reduce((result, curr, index) => {
-    const reverseWord = curr.split('').reverse().join('');
+    const reverseWord = curr.split("").reverse().join("");
     if (obj[reverseWord] > -1 && index !== obj[reverseWord]) {
         obj[curr] = obj[reverseWord];
         result.push([curr, reverseWord]);
@@ -21,10 +21,10 @@ const anagramms = arrWords.reduce((result, curr, index) => {
 console.log(anagramms);
 
 //Общий случай с анаграммами, когда из слова можно получить другое слово
-const arrWords1 = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'озв', 'кот', 'топ', 'апорт', 'тропа', 'дерево'];
+const arrWords1 = ["пот", "лом", "ток", "мол", "зов", "воз", "озв", "кот", "топ", "апорт", "тропа", "дерево"];
 const anagramms1 = arrWords1
     .map((value) => {
-        return value.split('').sort().join('');
+        return value.split("").sort().join("");
     })
     .reduce((result, curr, index, arr) => {
         const findInd = arr.findIndex((value) => value === curr);
@@ -34,11 +34,11 @@ const anagramms1 = arrWords1
 console.log(anagramms1);
 
 //Хавролевский вариант
-const arrWords2 = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'кот', 'топ', 'корыто', 'озв'];
+const arrWords2 = ["пот", "лом", "ток", "мол", "зов", "воз", "кот", "топ", "корыто", "озв"];
 
 function getAnograms(array) {
     const anograms = array.reduce((acc, word) => {
-        const orderedCharacters = word.split('').sort().join('');
+        const orderedCharacters = word.split("").sort().join("");
 
         if (acc[orderedCharacters]) {
             acc[orderedCharacters].push(word);
@@ -55,29 +55,27 @@ function getAnograms(array) {
 console.log(getAnograms(arrWords2));
 
 //Общий случай с анаграммами, когда из слова можно получить другое слово
-const arrWords3 = ['пот', 'лом', 'ток', 'мол', 'зов', 'воз', 'озв', 'кот', 'топ', 'апорт', 'тропа', 'дерево'];
+const arrWords3 = ["пот", "лом", "ток", "мол", "зов", "воз", "озв", "кот", "топ", "апорт", "тропа", "дерево"];
+
 const anagramms3 = arrWords3
     .map((value) => {
-        return value.split('').sort().join('');
+        return value.split("").sort().join("");
     })
-    .reduce((result, curr, index, arr) => {
-        const findInd = arr.reduce((acc, value, ind) => {}, []);
-        return result;
-    }, []);
-console.log(anagramms1);
+    .sort();
+console.log(anagramms3);
 
 //Задача № 2.
 //Палиндром. Проверить фразу на палиндром
 
-const word = 'калаш';
-const isPalindrom = word === word.split('').reverse().join('');
-console.log(isPalindrom ? 'Слово-палиндром' : 'Слово не является палиндромом');
+const word = "калаш";
+const isPalindrom = word === word.split("").reverse().join("");
+console.log(isPalindrom ? "Слово-палиндром" : "Слово не является палиндромом");
 
 //второй вариант
-const word1 = 'Лёша на полке клопа нашёл';
-const formatWord = word1.toLowerCase().split(' ').join('');
-const isPalindrom1 = formatWord === formatWord.split('').reverse().join('');
-console.log(isPalindrom1 ? 'Фраза-палиндром' : 'Фраза не является палиндромом');
+const word1 = "Лёша на полке клопа нашёл";
+const formatWord = word1.toLowerCase().split(" ").join("");
+const isPalindrom1 = formatWord === formatWord.split("").reverse().join("");
+console.log(isPalindrom1 ? "Фраза-палиндром" : "Фраза не является палиндромом");
 
 //Задача № 3.
 //(03).Купюры;
@@ -130,3 +128,46 @@ function exchangeBanknotes(sum) {
 }
 
 const result1 = exchangeBanknotes(summa);
+
+//Вариант Хавроля
+const sum = 7990;
+const banknotes1 = [5000, 500, 1000, 100, 50, 10];
+
+function getExcangeBancknotes(sum, banknotes1) {
+    let balance = sum;
+    const sortedBanknotes = banknotes1.sort((a, b) => b - a);
+
+    if (sum % sortedBanknotes[sortedBanknotes.length - 1]) {
+        return undefined;
+    }
+
+    return sortedBanknotes.reduce((acc, banknote) => {
+        acc[banknote] = Math.floor(balance / banknote);
+        balance = balance - acc[banknote] * banknote;
+
+        return acc;
+    }, {});
+}
+
+console.log(getExcangeBancknotes(sum, banknotes1));
+
+//5-я задача 'AAABBBCCD'=>'A3B3C2D'
+
+str = "AAABBBBCCD";
+function getNumberRepeatSymbols(str) {
+    const objSymbols = str.split("").reduce((result, symbol) => {
+        if (result[symbol]) {
+            result[symbol] = result[symbol] + 1;
+        } else {
+            result[symbol] = 1;
+        }
+        return result;
+    }, {});
+    return Object.entries(objSymbols)
+        .map(([key, value]) => {
+            return key + value;
+        })
+        .join("");
+}
+
+console.log(getNumberRepeatSymbols(str));
