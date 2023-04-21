43df1b28637f339d90a8b0fdca82e363653cd22e
Sergey Kuznetsov <mr.enline@gmail.com>
2023-04-21 14:29:54 +0300

    new task

--- js/script7.js
+++ js/script7.js
@@ -3,7 +3,7 @@
 //Усложнение: решить с `log n` сложностью.
 
 const arrSortNumbers = [1, 4, 10, 12, 22, 34, 34, 34, 34, 34, 34, 78, 100];
-const arrNumbers = [10, 34];
+const arrNumbers = [10, 34, 25];
 
 function getIndexSimple(arrNumbers) {
     const objIndex = {};
@@ -20,7 +20,7 @@ function getIndexSimple(arrNumbers) {
             return acc;
         }, -1);
         if (objIndex[num].length === 1) {
-            objIndex[num].push();
+            objIndex[num].push(objIndex[num][0]);
         }
     }
     return objIndex;
@@ -28,42 +28,78 @@ function getIndexSimple(arrNumbers) {
 
 console.log(getIndexSimple(arrNumbers));
 
-function searchFirstIndex(number) {
-    let startIndex = -1;
-    let endIndex = arrSortNumbers.length - 1;
+function searchIndexes(number) {
+    const objIndex = {};
 
-    // while (startIndex + 1 < endIndex) {
-    //     const index = startIndex + Math.floor((endIndex - startIndex) / 2);
-    //     if (arrSortNumbers[index] < number) {
-    //         startIndex = index;
-    //     } else {
-    //         endIndex = index;
-    //     }
-    // }
-
-    // if (arrSortNumbers[endIndex] === number) {
-    //     return endIndex;
-    // }
-    // return -1;
     if (number.length < 1) {
         return;
     }
 
     for (let i = 0; i < number.length; i++) {
-        while (startIndex + 1 < endIndex) {
-            const index = startIndex + Math.floor((endIndex - startIndex) / 2);
-            if (arrSortNumbers[index] < number[i]) {
-                startIndex = index;
-            } else {
-                endIndex = index;
-            }
+        if (objIndex[number[i]]) {
+            continue;
         }
 
-        if (arrSortNumbers[endIndex] === number[i]) {
-            return endIndex;
+        objIndex[number[i]] = [];
+
+        const firstIndex = getFirstIndex(number[i]);
+
+        if (firstIndex === -1) {
+            continue;
         }
-        return -1;
+
+        objIndex[number[i]].push(firstIndex);
+
+        const lastIndex = getLastIndex(number[i]);
+
+        if (lastIndex === -1) {
+            objIndex[number[i]].push(firstIndex);
+        }
+
+        objIndex[number[i]].push(lastIndex);
     }
+
+    return objIndex;
 }
 
-console.log(searchFirstIndex(arrNumbers));
+function getFirstIndex(number) {
+    let startIndex = -1;
+    let endIndex = arrSortNumbers.length - 1;
+
+    while (startIndex + 1 < endIndex) {
+        const index = startIndex + Math.floor((endIndex - startIndex) / 2);
+        if (arrSortNumbers[index] < number) {
+            startIndex = index;
+        } else {
+            endIndex = index;
+        }
+    }
+
+    if (arrSortNumbers[endIndex] === number) {
+        return endIndex;
+    }
+
+    return -1;
+}
+
+function getLastIndex(number) {
+    startIndex = 0;
+    endIndex = arrSortNumbers.length;
+
+    while (startIndex + 1 < endIndex) {
+        const index = startIndex + Math.floor((endIndex - startIndex) / 2);
+        if (arrSortNumbers[index] <= number) {
+            startIndex = index;
+        } else {
+            endIndex = index;
+        }
+    }
+
+    if (arrSortNumbers[startIndex] === number) {
+        return startIndex;
+    }
+
+    return -1;
+}
+
+console.log(searchIndexes(arrNumbers));
